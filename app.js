ecb21f88bc43f6b9b2c12d8bfc9839bc4156dcad
AlaeddineBenouadah <mabenouadah@gmail.com>
2025-11-13 12:09:47 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+// Variable que notre test CI doit trouver
+const VERSION_PROJET = "1.0.0"; 
+
+// Affiche le message sur la page
+document.getElementById('message').innerText = "Version du projet : " + VERSION_PROJET;
